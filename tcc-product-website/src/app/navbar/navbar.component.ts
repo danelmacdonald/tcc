9c27d982fd6e5b12bdf8cf937f78ebcb2397d130
Danel Mac Donald <danel.macdonald@empiriq.com>
2026-07-26 11:27:33 +0200

    Adds colours and start of navbar.

---
+++ tcc-product-website/src/app/navbar/navbar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent {
+  isMobileMenuOpen = false;
+
+  navLinks = [
+    { label: 'Home', href: '#' },
+    { label: 'About', href: '#' },
+    { label: 'Services', href: '#' },
+    { label: 'Contact', href: '#' },
+  ];
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+}
