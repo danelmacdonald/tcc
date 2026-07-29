84d287cbded9f332cdf94bccb9af598c2d147b48
Danel Mac Donald <danel.macdonald@empiriq.com>
2026-07-29 17:05:01 +0200

    Adds footer and banner component.

--- tcc-product-website/src/app/app.component.ts
+++ tcc-product-website/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
+import { BannerComponent } from './banner/banner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
